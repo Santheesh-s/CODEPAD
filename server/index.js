d7b25104ed6ac2f91624c4a85a21293d30116b9b
santheesh-s <santheesh24@gmail.com>
2026-02-16 10:09:23 -0500

    i think this is last

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ app.get('/api/new-room', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
 });
 
